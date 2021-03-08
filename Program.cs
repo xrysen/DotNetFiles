06c6b01d8a6ae57dde1ada5faca3f058050ca3bc
Sean Oyler <xrysen@gmail.com>
2021-03-08 00:51:49 -0800

    Scans all sales files for totals and exports them into a salesTotal file

--- Program.cs
+++ Program.cs
@@ -1,39 +1,70 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace files_module
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var currentDirectory = Directory.GetCurrentDirectory();
-            var storesDirectory = Path.Combine(currentDirectory, "stores");
-            
-            var salesFiles = FindFiles("stores");
+      var currentDirectory = Directory.GetCurrentDirectory();
+      var storesDirectory = Path.Combine(currentDirectory, "stores");
 
-            foreach (var file in salesFiles)
-            {
-                Console.WriteLine(file);
-            }
-        }
+      var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+      Directory.CreateDirectory(salesTotalDir);
 
-        static IEnumerable<string> FindFiles(string folderName)
-        {
-            List<string> salesFiles = new List<string>();
-            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+      var salesFiles = FindFiles(storesDirectory);
 
-            foreach (var file in foundFiles)
-            {
-                var extension = Path.GetExtension(file);
+      var salesTotal = CalculateSalesTotal(salesFiles);
 
-                if (extension == ".json")
-                {
-                    salesFiles.Add(file);
-                }
-            }
-            return salesFiles;
-        }
+      File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
     }
+
+    static IEnumerable<string> FindFiles(string folderName)
+    {
+      List<string> salesFiles = new List<string>();
+
+      var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+      foreach (var file in foundFiles)
+      {
+        var extension = Path.GetExtension(file);
+
+        if (extension == ".json")
+        {
+          salesFiles.Add(file);
+        }
+      }
+
+      return salesFiles;
+    }
+
+    static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+    {
+      double salesTotal = 0;
+
+      // Loop over each file path in salesFiles
+      foreach (var file in salesFiles)
+      {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data.Total;
+      }
+
+      return salesTotal;
+    }
+
+    class SalesData
+    {
+      public double Total { get; set; }
+    }
+
+  }
 }
