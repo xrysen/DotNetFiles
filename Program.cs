68129c1870919882883c20f2ac6230ee90899956
Sean Oyler <xrysen@gmail.com>
2021-03-07 21:21:22 -0800

    Adds FindFiles function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace files_module
 {
@@ -6,7 +8,27 @@ namespace files_module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var salesFiles = FindFiles("stores");
+
+            foreach (var file in salesFiles)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+            return salesFiles;
         }
     }
 }
